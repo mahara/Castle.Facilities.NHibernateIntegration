e594f803b83a126e242aaf2042c893cea7b4e125
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2019-05-14 00:19:35 +0700

    Upgraded to SDK-style .NET projects (part b).

--- src/Castle.Facilities.NHibernateIntegration.Tests/Internals/CallContextSessionStoreTestCase.cs
+++ src/Castle.Facilities.NHibernateIntegration.Tests/Internals/CallContextSessionStoreTestCase.cs
@@ -1,216 +1,214 @@
 #region License
-
-//  Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2019 Castle Project - https://www.castleproject.org/
 //
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-//
-
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #endregion
+
+using System;
+using System.Threading;
+
+using NHibernate;
+
+using NUnit.Framework;
 
 namespace Castle.Facilities.NHibernateIntegration.Tests.Internals
 {
-	using System;
-	using System.Threading;
-	using NHibernate;
-	using NUnit.Framework;
-	using SessionStores;
+    /// <summary>
+    /// Tests the CallContextSessionStore
+    /// </summary>
+    [TestFixture]
+    public class CallContextSessionStoreTestCase : AbstractNHibernateTestCase
+    {
+        private AutoResetEvent arEvent = new AutoResetEvent(false);
+        private AutoResetEvent _event = new AutoResetEvent(false);
 
-	/// <summary>
-	/// Tests the CallContextSessionStore
-	/// </summary>
-	[TestFixture]
-	public class CallContextSessionStoreTestCase : AbstractNHibernateTestCase
-	{
-		private AutoResetEvent arEvent = new AutoResetEvent(false);
-		private AutoResetEvent _event = new AutoResetEvent(false);
+        protected override string ConfigurationFile
+        {
+            get { return "Internals/CallContextSessionStoreConfiguration.xml"; }
+        }
 
-		protected override string ConfigurationFile
-		{
-			get { return "Internals/CallContextSessionStoreConfiguration.xml"; }
-		}
+        [Test]
+        public void NullAlias()
+        {
+            ISessionStore store = container.Resolve<ISessionStore>();
+            Assert.Throws<ArgumentNullException>(() => store.FindCompatibleSession(null));
+        }
 
-		[Test]
-		public void NullAlias()
-		{
-			ISessionStore store = container.Resolve<ISessionStore>();
-			Assert.Throws<ArgumentNullException>(() => store.FindCompatibleSession(null));
-		}
+        [Test]
+        public void FindCompatibleSession()
+        {
+            ISessionStore store = container.Resolve<ISessionStore>();
+            ISessionFactory factory = container.Resolve<ISessionFactory>();
 
-		[Test]
-		public void FindCompatibleSession()
-		{
-			ISessionStore store = container.Resolve<ISessionStore>();
-			ISessionFactory factory = container.Resolve<ISessionFactory>();
+            ISession session = store.FindCompatibleSession(Constants.DefaultAlias);
 
-			ISession session = store.FindCompatibleSession(Constants.DefaultAlias);
+            Assert.IsNull(session);
 
-			Assert.IsNull(session);
+            session = factory.OpenSession();
 
-			session = factory.OpenSession();
+            SessionDelegate sessDelegate = new SessionDelegate(true, session, store);
 
-			SessionDelegate sessDelegate = new SessionDelegate(true, session, store);
+            store.Store(Constants.DefaultAlias, sessDelegate);
 
-			store.Store(Constants.DefaultAlias, sessDelegate);
+            Assert.IsNotNull(sessDelegate.SessionStoreCookie);
 
-			Assert.IsNotNull(sessDelegate.SessionStoreCookie);
+            ISession session2 = store.FindCompatibleSession("something in the way she moves");
 
-			ISession session2 = store.FindCompatibleSession("something in the way she moves");
+            Assert.IsNull(session2);
 
-			Assert.IsNull(session2);
+            session2 = store.FindCompatibleSession(Constants.DefaultAlias);
 
-			session2 = store.FindCompatibleSession(Constants.DefaultAlias);
+            Assert.IsNotNull(session2);
+            Assert.AreSame(sessDelegate, session2);
 
-			Assert.IsNotNull(session2);
-			Assert.AreSame(sessDelegate, session2);
+            session.Dispose();
 
-			session.Dispose();
+            store.Remove(sessDelegate);
 
-			store.Remove(sessDelegate);
+            session = store.FindCompatibleSession(Constants.DefaultAlias);
 
-			session = store.FindCompatibleSession(Constants.DefaultAlias);
+            Assert.IsNull(session);
 
-			Assert.IsNull(session);
+            Assert.IsTrue(store.IsCurrentActivityEmptyFor(Constants.DefaultAlias));
+        }
 
-			Assert.IsTrue(store.IsCurrentActivityEmptyFor(Constants.DefaultAlias));
-		}
+        [Test]
+        public void FindCompatibleSessionWithTwoThreads()
+        {
+            ISessionStore store = container.Resolve<ISessionStore>();
+            ISessionFactory factory = container.Resolve<ISessionFactory>();
 
-		[Test]
-		public void FindCompatibleSessionWithTwoThreads()
-		{
-			ISessionStore store = container.Resolve<ISessionStore>();
-			ISessionFactory factory = container.Resolve<ISessionFactory>();
+            ISession session = factory.OpenSession();
 
-			ISession session = factory.OpenSession();
+            SessionDelegate sessDelegate = new SessionDelegate(true, session, store);
 
-			SessionDelegate sessDelegate = new SessionDelegate(true, session, store);
+            store.Store(Constants.DefaultAlias, sessDelegate);
 
-			store.Store(Constants.DefaultAlias, sessDelegate);
+            ISession session2 = store.FindCompatibleSession(Constants.DefaultAlias);
 
-			ISession session2 = store.FindCompatibleSession(Constants.DefaultAlias);
+            Assert.IsNotNull(session2);
+            Assert.AreSame(sessDelegate, session2);
 
-			Assert.IsNotNull(session2);
-			Assert.AreSame(sessDelegate, session2);
+            Thread newThread = new Thread(FindCompatibleSessionOnOtherThread);
+            newThread.Start();
 
-			Thread newThread = new Thread(FindCompatibleSessionOnOtherThread);
-			newThread.Start();
+            arEvent.WaitOne();
 
-			arEvent.WaitOne();
+            sessDelegate.Dispose();
 
-			sessDelegate.Dispose();
+            Assert.IsTrue(store.IsCurrentActivityEmptyFor(Constants.DefaultAlias));
+        }
 
-			Assert.IsTrue(store.IsCurrentActivityEmptyFor(Constants.DefaultAlias));
-		}
+        private void FindCompatibleSessionOnOtherThread()
+        {
+            ISessionStore store = container.Resolve<ISessionStore>();
 
-		private void FindCompatibleSessionOnOtherThread()
-		{
-			ISessionStore store = container.Resolve<ISessionStore>();
+            ISession session = store.FindCompatibleSession("something in the way she moves");
 
-			ISession session = store.FindCompatibleSession("something in the way she moves");
+            Assert.IsNull(session);
 
-			Assert.IsNull(session);
+            ISession session2 = store.FindCompatibleSession(Constants.DefaultAlias);
 
-			ISession session2 = store.FindCompatibleSession(Constants.DefaultAlias);
+            Assert.IsNull(session2);
 
-			Assert.IsNull(session2);
+            arEvent.Set();
+        }
 
-			arEvent.Set();
-		}
+        [Test]
+        public void NullAliasStateless()
+        {
+            ISessionStore store = container.Resolve<ISessionStore>();
+            Assert.Throws<ArgumentNullException>(() => store.FindCompatibleStatelessSession(null));
+        }
 
-		[Test]
-		public void NullAliasStateless()
-		{
-			ISessionStore store = container.Resolve<ISessionStore>();
-			Assert.Throws<ArgumentNullException>(() => store.FindCompatibleStatelessSession(null));
-		}
+        [Test]
+        public void FindCompatibleStatelessSession()
+        {
+            ISessionStore store = container.Resolve<ISessionStore>();
+            ISessionFactory factory = container.Resolve<ISessionFactory>();
 
-		[Test]
-		public void FindCompatibleStatelessSession()
-		{
-			ISessionStore store = container.Resolve<ISessionStore>();
-			ISessionFactory factory = container.Resolve<ISessionFactory>();
+            IStatelessSession session = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
 
-			IStatelessSession session = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
+            Assert.IsNull(session);
 
-			Assert.IsNull(session);
+            session = factory.OpenStatelessSession();
 
-			session = factory.OpenStatelessSession();
+            StatelessSessionDelegate sessionDelegate = new StatelessSessionDelegate(true, session, store);
 
-			StatelessSessionDelegate sessionDelegate = new StatelessSessionDelegate(true, session, store);
+            store.Store(Constants.DefaultAlias, sessionDelegate);
 
-			store.Store(Constants.DefaultAlias, sessionDelegate);
+            Assert.IsNotNull(sessionDelegate.SessionStoreCookie);
 
-			Assert.IsNotNull(sessionDelegate.SessionStoreCookie);
+            IStatelessSession session2 = store.FindCompatibleStatelessSession("something in the way she moves");
 
-			IStatelessSession session2 = store.FindCompatibleStatelessSession("something in the way she moves");
+            Assert.IsNull(session2);
 
-			Assert.IsNull(session2);
+            session2 = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
 
-			session2 = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
+            Assert.IsNotNull(session2);
+            Assert.AreSame(sessionDelegate, session2);
 
-			Assert.IsNotNull(session2);
-			Assert.AreSame(sessionDelegate, session2);
+            session.Dispose();
 
-			session.Dispose();
+            store.Remove(sessionDelegate);
 
-			store.Remove(sessionDelegate);
+            session = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
 
-			session = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
+            Assert.IsNull(session);
 
-			Assert.IsNull(session);
+            Assert.IsTrue(store.IsCurrentActivityEmptyFor(Constants.DefaultAlias));
+        }
 
-			Assert.IsTrue(store.IsCurrentActivityEmptyFor(Constants.DefaultAlias));
-		}
+        [Test]
+        public void FindCompatibleStatelessSessionWithTwoThreads()
+        {
+            ISessionStore store = container.Resolve<ISessionStore>();
+            ISessionFactory factory = container.Resolve<ISessionFactory>();
 
-		[Test]
-		public void FindCompatibleStatelessSessionWithTwoThreads()
-		{
-			ISessionStore store = container.Resolve<ISessionStore>();
-			ISessionFactory factory = container.Resolve<ISessionFactory>();
+            IStatelessSession session = factory.OpenStatelessSession();
 
-			IStatelessSession session = factory.OpenStatelessSession();
+            StatelessSessionDelegate sessionDelegate = new StatelessSessionDelegate(true, session, store);
 
-			StatelessSessionDelegate sessionDelegate = new StatelessSessionDelegate(true, session, store);
+            store.Store(Constants.DefaultAlias, sessionDelegate);
 
-			store.Store(Constants.DefaultAlias, sessionDelegate);
+            IStatelessSession session2 = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
 
-			IStatelessSession session2 = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
+            Assert.IsNotNull(session2);
+            Assert.AreSame(sessionDelegate, session2);
 
-			Assert.IsNotNull(session2);
-			Assert.AreSame(sessionDelegate, session2);
+            Thread newThread = new Thread(FindCompatibleStatelessSessionOnOtherThread);
+            newThread.Start();
 
-			Thread newThread = new Thread(FindCompatibleStatelessSessionOnOtherThread);
-			newThread.Start();
+            arEvent.WaitOne();
 
-			arEvent.WaitOne();
+            sessionDelegate.Dispose();
 
-			sessionDelegate.Dispose();
+            Assert.IsTrue(store.IsCurrentActivityEmptyFor(Constants.DefaultAlias));
+        }
 
-			Assert.IsTrue(store.IsCurrentActivityEmptyFor(Constants.DefaultAlias));
-		}
+        private void FindCompatibleStatelessSessionOnOtherThread()
+        {
+            ISessionStore store = container.Resolve<ISessionStore>();
 
-		private void FindCompatibleStatelessSessionOnOtherThread()
-		{
-			ISessionStore store = container.Resolve<ISessionStore>();
+            IStatelessSession session = store.FindCompatibleStatelessSession("something in the way she moves");
 
-			IStatelessSession session = store.FindCompatibleStatelessSession("something in the way she moves");
+            Assert.IsNull(session);
 
-			Assert.IsNull(session);
+            IStatelessSession session2 = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
 
-			IStatelessSession session2 = store.FindCompatibleStatelessSession(Constants.DefaultAlias);
+            Assert.IsNull(session2);
 
-			Assert.IsNull(session2);
-
-			arEvent.Set();
-		}
-	}
+            arEvent.Set();
+        }
+    }
 }
--- src/Castle.Facilities.NHibernateIntegration/Internal/SessionFactoryActivator.cs
+++ src/Castle.Facilities.NHibernateIntegration/Internal/SessionFactoryActivator.cs
@@ -1,81 +1,80 @@
-﻿#region License
-
-//  Copyright 2004-2010 Castle Project - http://www.castleproject.org/
-//  
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//  
-//      http://www.apache.org/licenses/LICENSE-2.0
-//  
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-// 
-
+#region License
+// Copyright 2004-2019 Castle Project - https://www.castleproject.org/
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #endregion
+
+using Castle.Core;
+
+using Castle.MicroKernel;
+using Castle.MicroKernel.ComponentActivator;
+using Castle.MicroKernel.Context;
+
+using NHibernate;
+using NHibernate.Cfg;
 
 namespace Castle.Facilities.NHibernateIntegration.Internal
 {
-	using Core;
-	using MicroKernel;
-	using MicroKernel.ComponentActivator;
-	using MicroKernel.Context;
-	using NHibernate;
-	using NHibernate.Cfg;
+    /// <summary>
+    /// Postpones the initiation of SessionFactory until Resolve
+    /// </summary>
+    public class SessionFactoryActivator : DefaultComponentActivator
+    {
+        /// <summary>
+        /// Constructor for SessionFactoryActivator
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="kernel"></param>
+        /// <param name="onCreation"></param>
+        /// <param name="onDestruction"></param>
+        public SessionFactoryActivator(ComponentModel model,
+                                       IKernelInternal kernel,
+                                       ComponentInstanceDelegate onCreation,
+                                       ComponentInstanceDelegate onDestruction)
+            : base(model, kernel, onCreation, onDestruction)
+        {
+        }
 
-	/// <summary>
-	/// Postpones the initiation of SessionFactory until Resolve
-	/// </summary>
-	public class SessionFactoryActivator : DefaultComponentActivator
-	{
-		/// <summary>
-		/// Constructor for SessionFactoryActivator
-		/// </summary>
-		/// <param name="model"></param>
-		/// <param name="kernel"></param>
-		/// <param name="onCreation"></param>
-		/// <param name="onDestruction"></param>
-		public SessionFactoryActivator(ComponentModel model,
-		                               IKernelInternal kernel,
-		                               ComponentInstanceDelegate onCreation,
-		                               ComponentInstanceDelegate onDestruction)
-			: base(model, kernel, onCreation, onDestruction)
-		{
-		}
+        /// <summary>
+        /// Calls the contributors
+        /// </summary>
+        protected virtual void RaiseCreatingSessionFactory()
+        {
+            var configuration = Model.ExtendedProperties[Constants.SessionFactoryConfiguration] as Configuration;
+            var contributors = Kernel.ResolveAll<IConfigurationContributor>();
+            foreach (var contributor in contributors)
+            {
+                contributor.Process(Model.Name, configuration);
+            }
+        }
 
-		/// <summary>
-		/// Calls the contributors
-		/// </summary>
-		protected virtual void RaiseCreatingSessionFactory()
-		{
-			var configuration = Model.ExtendedProperties[Constants.SessionFactoryConfiguration] as Configuration;
-			var contributors = Kernel.ResolveAll<IConfigurationContributor>();
-			foreach (var contributor in contributors)
-			{
-				contributor.Process(Model.Name, configuration);
-			}
-		}
+        /// <summary>
+        /// Creates the <see cref="ISessionFactory"/> from the configuration
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="burden"></param>
+        /// <returns></returns>
+        public override object Create(CreationContext context, Burden burden)
+        {
+            RaiseCreatingSessionFactory();
+            var configuration = this.Model.ExtendedProperties[Constants.SessionFactoryConfiguration]
+                                as Configuration;
 
-		/// <summary>
-		/// Creates the <see cref="ISessionFactory"/> from the configuration
-		/// </summary>
-		/// <param name="context"></param>
-		/// <param name="burden"></param>
-		/// <returns></returns>
-		public override object Create(CreationContext context, Burden burden)
-		{
-			RaiseCreatingSessionFactory();
-			var configuration = this.Model.ExtendedProperties[Constants.SessionFactoryConfiguration]
-			                    as Configuration;
-			
-			var sessionFactory = configuration.BuildSessionFactory();
+            var sessionFactory = configuration.BuildSessionFactory();
 
-			burden.SetRootInstance(sessionFactory);
+            burden.SetRootInstance(sessionFactory);
 
-			return sessionFactory;
-		}
-	}
+            return sessionFactory;
+        }
+    }
 }
