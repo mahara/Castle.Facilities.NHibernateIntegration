02d15e933920c2882297718d3d9051b7da198dda
Maximilian Haru Raditya <m4h4r4@gmail.com>
2019-05-13 22:44:44 +0700

    Improved build scripts.

--- buildscripts/BuildPackages.cmd
+++ buildscripts/BuildPackages.cmd
@@ -26,24 +26,24 @@ GOTO INITIALIZE_VARIABLES
 
 :INITIALIZE_VARIABLES
 SET CONFIGURATION="Release"
-SET BUILD_VERSION="4.0.0"
+SET BUILD_VERSION="5.0.0"
 
 GOTO SET_CONFIGURATION
 
 
 :SET_CONFIGURATION
-IF "%config%" == "" GOTO SET_BUILD_VERSION
+IF "%config%"=="" GOTO SET_BUILD_VERSION
 SET CONFIGURATION=%config%
 
 GOTO SET_BUILD_VERSION
 
 
 :SET_BUILD_VERSION
-IF "%version%" == "" GOTO RESTORE_PACKAGES
+IF "%version%"=="" GOTO RESTORE_PACKAGES
 SET BUILD_VERSION=%version%
 
 ECHO ---------------------------------------------------
-REM ECHO Building a "%config%" package with version "%version%"...
+REM ECHO Building "%config%" packages with version "%version%"...
 ECHO Building "%CONFIGURATION%" packages with version "%BUILD_VERSION%"...
 ECHO ---------------------------------------------------
 
@@ -60,8 +60,8 @@ GOTO BUILD
 
 
 :BUILD
-dotnet build .\tools\Explicit.NuGet.Versions\Explicit.NuGet.Versions.sln
-dotnet build Castle.Facilities.NHibernateIntegration.sln -c %CONFIGURATION% /p:APPVEYOR_BUILD_VERSION=%BUILD_VERSION%
+dotnet build .\tools\Explicit.NuGet.Versions\Explicit.NuGet.Versions.sln --no-restore
+dotnet build Castle.Facilities.NHibernateIntegration.sln -c %CONFIGURATION% /p:APPVEYOR_BUILD_VERSION=%BUILD_VERSION% --no-restore
 
 GOTO TEST
 
@@ -72,15 +72,14 @@ ECHO ----------------
 ECHO Running Tests...
 ECHO ----------------
 
-dotnet test .\src\Castle.Facilities.NHibernateIntegration.Tests || exit /b 1
+dotnet test .\src\Castle.Facilities.NHibernateIntegration.Tests --no-restore || exit /b 1
 
-GOTO NUGET_EXPLICIT_VERSIONS
+REM GOTO NUGET_EXPLICIT_VERSIONS
 
 
-:NUGET_EXPLICIT_VERSIONS
+REM :NUGET_EXPLICIT_VERSIONS
 
-.\tools\Explicit.NuGet.Versions\build\nev.exe ".\build" "Castle.Services.Transaction"
-.\tools\Explicit.NuGet.Versions\build\nev.exe ".\build" "Castle.Facilities.AutoTx"
+REM .\tools\Explicit.NuGet.Versions\build\nev.exe ".\build" "Castle.Facilities.NHibernateIntegration"
 
 
 
