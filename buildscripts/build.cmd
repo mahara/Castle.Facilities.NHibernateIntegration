8e57e183edd109b2e24ad0ba1d477e700b3d0f11
Maximilian Haru Raditya <m4h4r4@gmail.com>
2019-05-14 17:39:52 +0700

    Minor changes.

--- buildscripts/build.cmd
+++ buildscripts/build.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM ****************************************************************************
-REM Copyright 2004-2013 Castle Project - http://www.castleproject.org/
+REM Copyright 2004-2019 Castle Project - http://www.castleproject.org/
 REM Licensed under the Apache License, Version 2.0 (the "License");
 REM you may not use this file except in compliance with the License.
 REM You may obtain a copy of the License at
@@ -28,16 +28,14 @@ GOTO restore_packages
 :restore_packages
 dotnet restore ./tools/Explicit.NuGet.Versions/Explicit.NuGet.Versions.csproj
 dotnet restore ./buildscripts/BuildScripts.csproj
-dotnet restore ./src/Castle.Services.Transaction/Castle.Services.Transaction.csproj
-dotnet restore ./src/Castle.Services.Transaction.Tests/Castle.Services.Transaction.Tests.csproj
-dotnet restore ./src/Castle.Facilities.AutoTx/Castle.Facilities.AutoTx.csproj
-dotnet restore ./src/Castle.Facilities.AutoTx.Tests/Castle.Facilities.AutoTx.Tests.csproj
+dotnet restore ./src/Castle.Facilities.NHibernateIntegration/Castle.Facilities.NHibernateIntegration.csproj
+dotnet restore ./src/Castle.Facilities.NHibernateIntegration.Tests/Castle.Facilities.NHibernateIntegration.Tests.csproj
 
 GOTO build
 
 :build
 dotnet build ./tools/Explicit.NuGet.Versions/Explicit.NuGet.Versions.sln
-dotnet build Castle.Transactions.sln -c %Configuration%
+dotnet build Castle.Facilities.NHibernateIntegration.sln -c %Configuration%
 GOTO test
 
 :test
@@ -46,15 +44,13 @@ echo -------------
 echo Running Tests
 echo -------------
 
-dotnet test src\Castle.Services.Transaction.Tests || exit /b 1
-dotnet test src\Castle.Facilities.AutoTx.Tests || exit /b 1
+dotnet test src\Castle.Facilities.NHibernateIntegration.Tests || exit /b 1
 
 GOTO nuget_explicit_versions
 
 :nuget_explicit_versions
 
-.\tools\Explicit.NuGet.Versions\build\nev.exe ".\build" "Castle.Services.Transaction"
-.\tools\Explicit.NuGet.Versions\build\nev.exe ".\build" "Castle.Facilities.AutoTx"
+.\tools\Explicit.NuGet.Versions\build\nev.exe ".\build" "Castle.Facilities.NHibernateIntegration"
 
 
 
