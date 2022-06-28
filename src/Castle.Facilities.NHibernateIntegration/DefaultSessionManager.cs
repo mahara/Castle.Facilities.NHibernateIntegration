2499fe29d760dd6a2117fa8bfca12e9f0204ff70
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2022-06-28 13:18:06 +0700

    Renamed IsolationMode to IsolationLevel.

--- src/Castle.Facilities.NHibernateIntegration/DefaultSessionManager.cs
+++ src/Castle.Facilities.NHibernateIntegration/DefaultSessionManager.cs
@@ -257,9 +257,9 @@ namespace Castle.Facilities.NHibernateIntegration
             return true;
         }
 
-        private static System.Data.IsolationLevel TranslateIsolationLevel(IsolationLevel isolationMode)
+        private static System.Data.IsolationLevel TranslateIsolationLevel(IsolationLevel isolationLevel)
         {
-            switch (isolationMode)
+            switch (isolationLevel)
             {
                 case IsolationLevel.Chaos:
                     return System.Data.IsolationLevel.Chaos;
