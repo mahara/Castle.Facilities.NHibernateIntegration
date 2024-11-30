7ac54f47c2ab34b6fd57a47d2a5e2efdfb97d11d
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-11-30 18:05:23 +0700

    Refactored code (collection expressions).

--- src/Castle.Facilities.NHibernateIntegration/DefaultSessionManager.cs
+++ src/Castle.Facilities.NHibernateIntegration/DefaultSessionManager.cs
@@ -147,7 +147,7 @@ namespace Castle.Facilities.NHibernateIntegration
 
             if (list is null)
             {
-                list = new List<ISession>();
+                list = [];
 
                 shouldEnlist = true;
             }
@@ -217,7 +217,7 @@ namespace Castle.Facilities.NHibernateIntegration
 
             if (list is null)
             {
-                list = new List<IStatelessSession>();
+                list = [];
 
                 shouldEnlist = true;
             }
