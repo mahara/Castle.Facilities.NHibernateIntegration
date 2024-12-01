1f8dcde8fa7f8d75a4dcc619c3378e282fb53081
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-12-01 19:02:15 +0700

    Upgraded to .NET 9.0.

--- CleanPackages.cmd
+++ CleanPackages.cmd
@@ -6,8 +6,8 @@
 IF NOT DEFINED ARTIFACTS_FOLDER_PATH EXIT /B 1
 
 
-REM dotnet clean "Castle.Facilities.NHibernateIntegration.sln" --configuration Debug
-REM dotnet clean "Castle.Facilities.NHibernateIntegration.sln" --configuration Release
+REM dotnet clean "Castle.Facilities.NHibernateIntegration.slnx" --configuration Debug
+REM dotnet clean "Castle.Facilities.NHibernateIntegration.slnx" --configuration Release
 
 IF EXIST "%ARTIFACTS_FOLDER_PATH%" (
     ECHO Deleting "%ARTIFACTS_FOLDER_PATH%" folder...
