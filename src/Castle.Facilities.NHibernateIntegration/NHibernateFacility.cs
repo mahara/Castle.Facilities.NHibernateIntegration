ba3905819ee0bf7c2e5020395ddf3bed768db020
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-12-02 20:55:56 +0700

    Refactored code.

--- src/Castle.Facilities.NHibernateIntegration/NHibernateFacility.cs
+++ src/Castle.Facilities.NHibernateIntegration/NHibernateFacility.cs
@@ -572,7 +572,7 @@ namespace Castle.Facilities.NHibernateIntegration
         {
             SessionFactoryFacilityConfigurations =
                 _configuration!.Children
-                               .Select(configuration => new NHibernateSessionFactoryFacilityConfiguration(configuration));
+                               .Select(static configuration => new NHibernateSessionFactoryFacilityConfiguration(configuration));
         }
 
         public Type GetSessionStoreType()
